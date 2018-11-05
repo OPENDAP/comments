b4725128fb8370ad0f6c8096becc8afad31cd81a
James Gallagher <jgallagher@opendap.org>
2018-11-04 21:38:26 -0700

    Checkpoint - add a feedback form.

---
+++ src/main/java/org/opendap/beans/FeedbackData.java
@@ -0,0 +1,32 @@
+
+/// The Model in the MVC example
+
+package org.opendap.beans;
+
+// This class comes from a Spring MVC example that uses JSP to build a 
+// HTML form.
+public class FeedbackData {
+   private Integer age;
+   private String name;
+   private Integer id;
+
+   public void setAge(Integer age) {
+      this.age = age;
+   }
+   public Integer getAge() {
+      return age;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setId(Integer id) {
+      this.id = id;
+   }
+   public Integer getId() {
+      return id;
+   }
+}
+   
