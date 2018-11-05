2752871bac369fe4d8426fe8b611aa88f801a5e9
James Gallagher <jgallagher@opendap.org>
2018-11-05 16:38:06 -0700

    checkpoint - test fails, but bootRun works

--- src/main/java/org/opendap/feedback/FeedbackApplication.java
+++ src/main/java/org/opendap/feedback/FeedbackApplication.java
@@ -1,5 +1,5 @@
 
-package org.opendap.comments;
+package org.opendap.feedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/opendap/feedback/FeedbackApplicationTests.java
+++ src/test/java/org/opendap/feedback/FeedbackApplicationTests.java
@@ -1,4 +1,4 @@
-package org.opendap.comments;
+package org.opendap.feedback;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
