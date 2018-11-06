c86e3e35992c6f0b421f93d62f7e9aa5c50f37f6
James Gallagher <jgallagher@opendap.org>
2018-11-06 16:31:50 -0700

    Added support for logging
    Really, it was there all along, but I had to learn that... Added support
    for simplelogger and log4j as well as the default (for Sprint Boot)
    ch.qos code.

--- src/main/java/org/opendap/rest/controller/FeedbackFormController.java
+++ src/main/java/org/opendap/rest/controller/FeedbackFormController.java
@@ -7,21 +7,25 @@ import org.opendap.beans.FeedbackForm;
 
 import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
 @Controller
 public class FeedbackFormController {
 
+	//private static final Logger log = LoggerFactory.getLogger(FeedbackFormController.class);
+	 
 	@RequestMapping(value = "/feedback/form", method = RequestMethod.GET)
 	public ModelAndView feedbackForm(@RequestParam(name = "url", required = false, defaultValue = "http://localhost:8080/opendap/") String url) {
 		FeedbackForm ffb = new FeedbackForm();
 		ffb.setUrl(url);
-		System.out.println("Before calling ModelAndView");
 		return new ModelAndView("feedback_form", "feedback_form_info", ffb);
 	}
 	
--- src/main/java/org/opendap/rest/controller/VersionController.java
+++ src/main/java/org/opendap/rest/controller/VersionController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @brief Sprint boot Controller for the response to /feedback/version
@@ -19,22 +24,29 @@ import org.springframework.beans.factory.annotation.Value;
 @Controller
 public class VersionController {
 
+	// Added to test SLF4j.
+	private static final Logger log = LoggerFactory.getLogger(FeedbackFormController.class);
+	private static final String version_key = "feedback.version";
+	
 	// Read feedback.version from the 'application.properties'. see
 	// http://www.appsdeveloperblog.com/reading-application-properties-spring-boot/
-	@Value("${feedback.version}")
-	private String version_number;
+	// @Value("${feedback.version}")
+	// private String version_number;
 	
-	private static final String versionmsg = "Feedback, version %s";
+	private static final String version_msg = "Feedback, version %s";
 
 	// This will inject an Environment object into this class. The Environment
 	// object stores the info in application.properties as key:value pairs. I
 	// used the @Value annotation. See the link above for more info.
-	// @Autowired Environment env;
+	@Autowired 
+	private Environment env;
 	
 	@GetMapping("/feedback/version")
 	@ResponseBody
 	public Version versionResponse() {
-		return new Version(String.format(versionmsg, version_number));
+		String version_number = env.getProperty(version_key);
+		log.debug("About to return version info, version: {}", version_number);	// Here to test slf4j
+		return new Version(String.format(version_msg, version_number));
 	}
 
 }
